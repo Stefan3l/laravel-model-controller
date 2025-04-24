761dd52438be3510391f7b5a9a7be5a979e9ab96
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-04-24 12:07:55 +0200

    init project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container py-4 ">
+        <h1 class=" text-primary text-center">Benvenuto in Laravel</h1>
+    </div>
+</body>
+</html>
