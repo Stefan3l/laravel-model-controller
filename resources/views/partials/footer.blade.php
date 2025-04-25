bb6e3a7f2a56f8ea4b634efcaaefc1adba88fbc8
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-04-25 09:40:37 +0200

    fix: footer and header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,5 @@
-<div class="container">
-    <h1 class="text-primary text-center py-5"> FOOTER</h1>
-</div>
+ <section class="bg-primary py-3 mt-5 shadow-xl ">   
+    <div class="container">
+        <h1 class="text-white text-center py-5"> FOOTER</h1>
+    </div>
+</section>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,12 @@
-<section class="bg-light py-3 shadow-xl ">
+<section class="bg-primary py-3 shadow-xl ">
     <div class="container">
         <header class="d-flex justify-content-between align-items-center py-3">
-            <h1 class="text-primary">Lista Movies</h1>
+            <h1 class="text-white">Lista Movies</h1>
             <nav>
-                <ul class="d-flex list-unstyled">
-                    <li class="mx-2"><a href="#" class="text-decoration-none">Home</a></li>
-                    <li class="mx-2"><a href="#" class="text-decoration-none">About</a></li>
-                    <li class="mx-2"><a href="#" class="text-decoration-none">Contact</a></li>
+                <ul class="d-flex list-unstyled ">
+                    <li class="mx-2"><a href="#" class="text-decoration-none fs-5 text-white">Home</a></li>
+                    <li class="mx-2"><a href="#" class="text-decoration-none fs-5 text-white">About</a></li>
+                    <li class="mx-2"><a href="#" class="text-decoration-none fs-5 text-white">Contact</a></li>
                 </ul>
             </nav>
         </header>
